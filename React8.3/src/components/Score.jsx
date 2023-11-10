6fb15b3f9fb3fa2ef755136c842861cd18506f01
Katriceb <birch5trice@gmail.com>
2023-11-10 11:10:32 -0500

    first comment

---
+++ React8.3/src/components/Score.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function score({scoreObjects}) {
+  return (
+    <div>
+        <> student score is :{scoreObjects.score}</>
+        <> student date is :{scoreObjects.date}</>
+        
+        </div>
+  )
+}
+
+export default score;
---
+++ React8.3/src/components/Students.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Score from '.components/score';
+
+function Students({props}) {
+
+}
+
+    return ( 
+   <div>
+    
+      <h1> students name: {studentObjects.name} </h1>
+      <h2> student bio: {studentObjects.bio}</h2>
+    
+      
+      
+           {/*{console.log(studentObjects)}*/}
+           {studentObjects.scores.map((score) => (
+        )
+            </div> 
+         
+     );
+         
+             
+            
+        
+        
+        
+
+
+export default Students;
